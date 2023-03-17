f0e874f63ddfc5a81ee9df7da7c8b21ed41b78e1
noah-asherbranner <laces@asu.edu>
2023-03-17 14:16:31 -0700

    updating styles and headers

--- server.js
+++ server.js
@@ -35,5 +35,5 @@ app.get('/software', (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log('Server listening on port ${PORT}');
 });
