5c7079518348d60b72fffe9b1815c0fcd6b1690a
noah-asherbranner <laces@asu.edu>
2023-03-22 23:03:41 -0700

    og and twitter card sharing should work!

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/software/html5', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
