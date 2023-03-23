4766cac4932cc24db37dc9cda907ccc3bb6bfec9
noah-asherbranner <laces@asu.edu>
2023-03-23 01:30:49 -0700

    final commit of the night, website secured.

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 
 // Require necessary modules
 const express = require('express');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 
 // Create a new instance of Express
 const app = express();
+app.use(express.static('public'));
 app.use(express.static(__dirname, { dotfiles: 'allow' } ));
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
+const privateKey = fs.readFileSync('C:/Certbot/live/web.noaha.tech/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('C:/Certbot/live/web.noaha.tech/cert.pem', 'utf8');
+const ca = fs.readFileSync('C:/Certbot/live/web.noaha.tech/chain.pem', 'utf8');
 
-// Serve static files from the public directory
-app.use(express.static('public'));
-
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 
 // Define routes
 app.get('/', (req, res) => {
@@ -57,7 +66,13 @@ app.get('/software/html5', (req, res) => {
   res.render('html5');
 });
 // Start the server
-const PORT = process.env.PORT || 80;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
 });
