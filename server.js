c15ea7c39eeb16259abfffbe95fef323986d3348
noah-asherbranner <laces@asu.edu>
2023-03-23 14:27:24 -0700

    ssl?

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
-
 // Require necessary modules
 const express = require('express');
 const fs = require('fs');
 const http = require('http');
 const https = require('https');
-
+const httpport = 80;
+const httpsport = 443;
 // Create a new instance of Express
 const app = express();
+app.use((req, res, next) => {
+  if(req.protocol === 'http') {
+    res.redirect(301, "https://" + req.headers.host + req.url);
+    res.end();
+  }
+  next();
+});
 app.use(express.static('public'));
-app.use(express.static(__dirname, { dotfiles: 'allow' } ));
+app.use(express.static(__dirname, { dotfiles: 'allow' }));
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
-const privateKey = fs.readFileSync('C:/Certbot/live/noaha.tech/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('C:/Certbot/live/noaha.tech/cert.pem', 'utf8');
-const ca = fs.readFileSync('C:/Certbot/live/noaha.tech/chain.pem', 'utf8');
-
+const privateKey = fs.readFileSync('C://Certbot/live/noaha.tech/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('C://Certbot/live/noaha.tech/cert.pem', 'utf8');
+const ca = fs.readFileSync('C://Certbot/live/noaha.tech/chain.pem', 'utf8');
 const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
+  key: privateKey,
+  cert: certificate,
+  ca: ca
 };
+
+// Start the server
+const httpServer = http.createServer(app); 
+const httpsServer = https.createServer(credentials, app);
+
 
 // Define routes
 app.get('/', (req, res) => {
@@ -65,14 +76,12 @@ app.get('/software/java', (req, res) => {
 app.get('/software/html5', (req, res) => {
   res.render('html5');
 });
-// Start the server
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app); 
 
-httpServer.listen(80, () => {
+httpServer.listen(httpport, () => {
 	console.log('HTTP Server running on port 80');
 });
 
-httpsServer.listen(443, () => {
+
+httpsServer.listen(httpsport,() => {
 	console.log('HTTPS Server running on port 443');
 });
