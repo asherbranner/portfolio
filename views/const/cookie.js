5f6eacb0887940e0f27c759f607be5b3d2431a54
noah-asherbranner <laces@asu.edu>
2023-04-07 23:57:36 -0700

    samesite cookies fixed, all data flows properly

--- views/const/cookie.js
+++ views/const/cookie.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
         "consent_type": "express",
         "palette": "dark",
         "language": "en",
-        "page_load_consent_levels": ["strictly-necessary","tracking"],
+        "page_load_consent_levels": ["strictly-necessary", "tracking"],
         "notice_banner_reject_button_hide": false,
         "preferences_center_close_button_hide": false,
         "page_refresh_confirmation_buttons": false,
@@ -22,5 +22,14 @@ document.addEventListener('DOMContentLoaded', function () {
     gtag('js', new Date());
     gtag('config', 'G-JRMXMCDZF9', 'auto', {
         cookie_flags: 'max-age=7200; secure=true; samesite=none'
-    });
+    })
+    gtag('config', 'G-0V7JNYGWCC', 'auto', {
+        cookie_flags: 'max-age=7200; secure=true; samesite=none'
+    })
+    gtag('config', 'G-B8SKQ9HHPZ', 'auto', {
+        cookie_flags: 'max-age=7200; secure=true; samesite=none'
+    })
+    gtag('config', 'G-X6LMX9VR0Y', 'auto', {
+        cookie_flags: 'max-age=7200; secure=true; samesite=none'
+    })
 });
